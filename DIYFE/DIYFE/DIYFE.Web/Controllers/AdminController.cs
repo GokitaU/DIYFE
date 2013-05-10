4344dbade6e54c20229861a6e55140f6d3f7dc2e
John Tipps <jtipps@hanleywood.com>
2013-05-10 07:42:25 -0500

    hey

---
+++ DIYFE/DIYFE/DIYFE.Web/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DIYFE.Web.Controllers
+{
+    public class AdminController : ApplicationController
+    {
+        //
+        // GET: /Admin/
+        public ActionResult Index()
+        {
+            DIYFE.Web.Models.AdminModel model = new DIYFE.Web.Models.AdminModel();
+            model.Categories = AppStatic.Categories;
+
+            PageModel.ArticleContent.Title = "";
+            PageModel.ArticleContent.Description = "";
+            PageModel.ArticleContent.Author = "";
+            PageModel.ArticleContent.Keywords = "";
+
+            return View(model);
+        }
+
+
+
+    }
+}
