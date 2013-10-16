8d9199efe3f92065d57e10c965656e90556cca1a
Sum Dood <sumdood31@gmail.com>
2013-10-16 08:16:04 -0500

    a few betting fix

--- DIYFE_old/DIYFE/DIYFE.Web/Global.asax.cs
+++ DIYFE_old/DIYFE/DIYFE.Web/Global.asax.cs
@@ -24,121 +24,121 @@ namespace DIYFE.Web
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            //routes.MapRoute(
-            //    "DefaultSportsNBA", // Route name
-            //    "sports-betting/{controller}/{action}/{id}", // URL with parameters
-            //    new { controller = "Sports", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            //);
+            routes.MapRoute(
+                "DefaultSportsNBA", // Route name
+                "sports-betting/{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Sports", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
 
             //routes.MapRoute(
             //    "Categories",
             //    "Categories/{*category}",
             //    new { controller = "Categories", action = "Details" });
 
-            #region ProjectMaps
+            //#region ProjectMaps
 
-            routes.MapRoute(
-                "ProjectRoot", // Route name
-                "Project", // URL with parameters
-                new { controller = "Project", action = "Index" } // Parameter defaults
-            );
+            //routes.MapRoute(
+            //    "ProjectRoot", // Route name
+            //    "Project", // URL with parameters
+            //    new { controller = "Project", action = "Index" } // Parameter defaults
+            //);
 
-            routes.MapRoute(
-                "ProjectContent", // Route name
-                "Project/{html}.html", // URL with parameters,
-                new { controller = "Project", action = "ProjectDetails" }//, // Parameter defaults
-            );
+            //routes.MapRoute(
+            //    "ProjectContent", // Route name
+            //    "Project/{html}.html", // URL with parameters,
+            //    new { controller = "Project", action = "ProjectDetails" }//, // Parameter defaults
+            //);
 
-            //---------------------------------------
-            routes.MapRoute(
-                "ProjectCategory", // Route name
-                "Project/{categoryUrl}", // URL with parameters
-                new { controller = "Project", action = "FirstLevCategoryList", categoryUrl = "" }
-            );
-            routes.MapRoute(
-                "ProjectContent1", // Route name
-                "Project/{categoryUrl}/{html}.html", // URL with parameters,
-                new { controller = "Project", action = "ProjectDetails" }//, // Parameter defaults
-            );
+            ////---------------------------------------
+            //routes.MapRoute(
+            //    "ProjectCategory", // Route name
+            //    "Project/{categoryUrl}", // URL with parameters
+            //    new { controller = "Project", action = "FirstLevCategoryList", categoryUrl = "" }
+            //);
+            //routes.MapRoute(
+            //    "ProjectContent1", // Route name
+            //    "Project/{categoryUrl}/{html}.html", // URL with parameters,
+            //    new { controller = "Project", action = "ProjectDetails" }//, // Parameter defaults
+            //);
 
-            //----------------------
-            routes.MapRoute(
-                "ProjectSubCategory", // Route name
-                "Project/{categoryUrl}/{subCategoryUrl}", // URL with parameters
-                new { controller = "Project", action = "SecondLevCategoryList", categoryUrl = "", subCategoryUrl = "" } // Parameter defaults
-            );
-            routes.MapRoute(
-                "ProjectContent2", // Route name
-                "Project/{categoryUrl}/{subCategoryUrl}/{html}.html", // URL with parameters,
-                new { controller = "Project", action = "ProjectDetails" }//, // Parameter defaults
-            );
+            ////----------------------
+            //routes.MapRoute(
+            //    "ProjectSubCategory", // Route name
+            //    "Project/{categoryUrl}/{subCategoryUrl}", // URL with parameters
+            //    new { controller = "Project", action = "SecondLevCategoryList", categoryUrl = "", subCategoryUrl = "" } // Parameter defaults
+            //);
+            //routes.MapRoute(
+            //    "ProjectContent2", // Route name
+            //    "Project/{categoryUrl}/{subCategoryUrl}/{html}.html", // URL with parameters,
+            //    new { controller = "Project", action = "ProjectDetails" }//, // Parameter defaults
+            //);
 
-            //-------------------
-            routes.MapRoute(
-                "ProjectSubSubCategory", // Route name
-                "Project/{categoryUrl}/{subCategoryUrl}/{subSubCategoryUrl}", // URL with parameters
-                new { controller = "Project", action = "ThirdLevCategoryList", categoryUrl = "", subCategoryUrl = "", subSubCategoryUrl = "" } // Parameter defaults
-            );
-            routes.MapRoute(
-                "ProjectContent3", // Route name
-                "Project/{categoryUrl}/{subCategoryUrl}/{subSubCategoryUrl}/{html}.html", // URL with parameters,
-                new { controller = "Project", action = "ProjectDetails" }//, // Parameter defaults
-            );
+            ////-------------------
+            //routes.MapRoute(
+            //    "ProjectSubSubCategory", // Route name
+            //    "Project/{categoryUrl}/{subCategoryUrl}/{subSubCategoryUrl}", // URL with parameters
+            //    new { controller = "Project", action = "ThirdLevCategoryList", categoryUrl = "", subCategoryUrl = "", subSubCategoryUrl = "" } // Parameter defaults
+            //);
+            //routes.MapRoute(
+            //    "ProjectContent3", // Route name
+            //    "Project/{categoryUrl}/{subCategoryUrl}/{subSubCategoryUrl}/{html}.html", // URL with parameters,
+            //    new { controller = "Project", action = "ProjectDetails" }//, // Parameter defaults
+            //);
             
-            #endregion
+            //#endregion
 
-            #region PostMaps
+            //#region PostMaps
 
-            routes.MapRoute(
-                "PostRoot", // Route name
-                "Post", // URL with parameters
-                new { controller = "Post", action = "Index" } // Parameter defaults
-            );
+            //routes.MapRoute(
+            //    "PostRoot", // Route name
+            //    "Post", // URL with parameters
+            //    new { controller = "Post", action = "Index" } // Parameter defaults
+            //);
 
-            routes.MapRoute(
-                "PostContent", // Route name
-                "Post/{html}.html", // URL with parameters,
-                new { controller = "Post", action = "PostDetails" }//, // Parameter defaults
-            );
+            //routes.MapRoute(
+            //    "PostContent", // Route name
+            //    "Post/{html}.html", // URL with parameters,
+            //    new { controller = "Post", action = "PostDetails" }//, // Parameter defaults
+            //);
 
-            //---------------------------------------
-            routes.MapRoute(
-                "PostCategory", // Route name
-                "Post/{categoryUrl}", // URL with parameters
-                new { controller = "Post", action = "FirstLevCategoryList", categoryUrl = "" }
-            );
+            ////---------------------------------------
+            //routes.MapRoute(
+            //    "PostCategory", // Route name
+            //    "Post/{categoryUrl}", // URL with parameters
+            //    new { controller = "Post", action = "FirstLevCategoryList", categoryUrl = "" }
+            //);
 
-            routes.MapRoute(
-                "PostContent1", // Route name
-                "Post/{categoryUrl}/{html}.html", // URL with parameters,
-                new { controller = "Post", action = "PostDetails" }//, // Parameter defaults
-            );
+            //routes.MapRoute(
+            //    "PostContent1", // Route name
+            //    "Post/{categoryUrl}/{html}.html", // URL with parameters,
+            //    new { controller = "Post", action = "PostDetails" }//, // Parameter defaults
+            //);
 
-            //----------------------
-            routes.MapRoute(
-                "PostSubCategory", // Route name
-                "Post/{categoryUrl}/{subCategoryUrl}", // URL with parameters
-                new { controller = "Post", action = "SecondLevCategoryList", categoryUrl = "", subCategoryUrl = "" } // Parameter defaults
-            );
-            routes.MapRoute(
-                "PostContent2", // Route name
-                "Post/{categoryUrl}/{subCategoryUrl}/{html}.html", // URL with parameters,
-                new { controller = "Post", action = "PostDetails" }//, // Parameter defaults
-            );
+            ////----------------------
+            //routes.MapRoute(
+            //    "PostSubCategory", // Route name
+            //    "Post/{categoryUrl}/{subCategoryUrl}", // URL with parameters
+            //    new { controller = "Post", action = "SecondLevCategoryList", categoryUrl = "", subCategoryUrl = "" } // Parameter defaults
+            //);
+            //routes.MapRoute(
+            //    "PostContent2", // Route name
+            //    "Post/{categoryUrl}/{subCategoryUrl}/{html}.html", // URL with parameters,
+            //    new { controller = "Post", action = "PostDetails" }//, // Parameter defaults
+            //);
 
-            //-------------------
-            routes.MapRoute(
-                "PostSubSubCategory", // Route name
-                "Post/{categoryUrl}/{subCategoryUrl}/{subSubCategoryUrl}", // URL with parameters
-                new { controller = "Post", action = "ThirdLevCategoryList", categoryUrl = "", subCategoryUrl = "", subSubCategoryUrl = "" } // Parameter defaults
-            );
-            routes.MapRoute(
-                "PostContent3", // Route name
-                "Post/{categoryUrl}/{subCategoryUrl}/{subSubCategoryUrl}/{html}.html", // URL with parameters,
-                new { controller = "Post", action = "PostDetails" }//, // Parameter defaults
-            );
+            ////-------------------
+            //routes.MapRoute(
+            //    "PostSubSubCategory", // Route name
+            //    "Post/{categoryUrl}/{subCategoryUrl}/{subSubCategoryUrl}", // URL with parameters
+            //    new { controller = "Post", action = "ThirdLevCategoryList", categoryUrl = "", subCategoryUrl = "", subSubCategoryUrl = "" } // Parameter defaults
+            //);
+            //routes.MapRoute(
+            //    "PostContent3", // Route name
+            //    "Post/{categoryUrl}/{subCategoryUrl}/{subSubCategoryUrl}/{html}.html", // URL with parameters,
+            //    new { controller = "Post", action = "PostDetails" }//, // Parameter defaults
+            //);
 
-            #endregion
+            //#endregion
 
             routes.MapRoute(
                 "Default", // Route name
