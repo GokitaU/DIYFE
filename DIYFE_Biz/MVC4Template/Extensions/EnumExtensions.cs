9f34ab8a1e081e761bad873c1146772ae2fb7d8e
John Tipps <jtipps@hanleywood.com>
2013-10-17 10:36:04 -0500

    merge DIY_Biz with master

---
+++ DIYFE_Biz/MVC4Template/Extensions/EnumExtensions.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Reflection;
+
+namespace DIYFEWeb.Common.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetStringValue(this Enum value)
+        {
+            Type type = value.GetType();
+            FieldInfo fieldInfo = type.GetField(value.ToString());
+
+            return "";
+        }
+    }
+}
---
+++ DIYFE_Biz/MVC4Template/Scripts/AppSupport/comment.js
@@ -0,0 +1,56 @@
+﻿$(document).ready(function () {
+
+    var commentTemplate =
+                    '<li class="sfcommentDetails" id="{0}">' +
+                            '<span class="sfcommentNumber">0</span>'
+    '<strong class="sfcommentAuthor">{1}</strong>' +
+    '<em class="sfcommentDate">{2}</em>' +
+    '<div class="sfcommentText">{3}</div>' +
+    '</li>';
+
+    $('#btnSubmitComment').click(function () {
+
+        var comment = {
+            ArticleId: $('#articleId').val(),
+            PosterName: $('#txtPosterName').val(),
+            PosterEmail: $('#txtPosterEmail').val(),
+            PosterWebSite: $('#txtPosterWebSite').val(),
+            Text: $('#txtCommentText').val()
+        }
+
+        if (comment.PosterName == '') {
+            alert('Post Name is required.');
+            return;
+        }
+        if (comment.Text == '') {
+            alert('Comment Text is required.');
+            return;
+        }
+
+        $.ajax({
+            url: GetRootURL() + 'Service/PostComment',
+            data: JSON.stringify(comment),
+            type: 'POST',
+            contentType: 'application/json; charset=utf-8',
+            success: function (data) {
+                if (data.success) {
+                    //loadingDiv.hide();
+                    //mediator.publish('BetLoadDone', {});
+                    var commentList = $('#commentsList');
+                    var addHtml = commentTemplate.format(data.obj.Id, data.obj.PosterName, data.obj.CreatedDate, data.obj.Text);
+                    commentList.prepend(addHtml);
+                    commentList.find('li [id=' + data.obj.Id + ']').focus();
+                    alert('remimber to add comment to page - tried to fix to to return the added object but enity framework doesnt return the id and the jquery framework doesnt support focus or it ist working correctly');
+                } else {
+                    //show error
+                    mediator.publish('pageError', { error: data.message, method: 'Insert Comment' });
+                }
+            },
+            error: function () {
+                mediator.publish('logError', { error: 'Error with service for InsertNBABetting', method: 'NBA Load Bets' });
+            }
+        });
+
+    });
+
+});
